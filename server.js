c0d46814e458193d568a1ed8247a542b99d0f1c4
Armaan Singh <08armaan21singh@gmail.com>
2025-09-12 17:20:45 -0500

    post request

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-
 app.use(express.static('public'))
+
+app.get('/getResponses', (req, res) => {
+  res.send('not implemented yet')
+})
+
+app.post('/postResponse', async (req, res) => {
+  res.send('POST request to the homepage')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
