a737b6f426fe313aea8aeefcb15438d049069acb
Armaan Singh <08armaan21singh@gmail.com>
2025-09-13 14:38:18 -0500

    simple system to tell you if the post was able to post or not

--- public/post.js
+++ public/post.js
@@ -9,13 +9,26 @@ async function sendData() {
       method: "POST",
       body: formData,
     })
-    console.log(await response.text())
+    const result = await response.text()
+    return result
   } catch (e) {
     console.error(e)
   }
 }
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault()
-  sendData()
+  const result = await sendData()
+  let notificationDiv = document.getElementById("notification")
+  notificationDiv.innerHTML = ""
+  console.log(result)
+  if (result == "posted successfully") {
+    notificationDiv.className = "notification"
+  } else {
+    notificationDiv.className = "notificationError"
+  }
+  let text = document.createElement("h1")
+  text.className = "notificationText"
+  let textNode = document.createTextNode(result)
+  notificationDiv.appendChild(text).appendChild(textNode)
 })
