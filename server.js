77868459bb8bb92063d72c8e419cef4b14cbbfa9
Armaan Singh <08armaan21singh@gmail.com>
2025-09-12 19:09:22 -0500

    db start

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 import express from 'express'
+import { JSONFilePreset } from 'lowdb/node'
+
+const defaultData = { posts: [] }
+const db = await JSONFilePreset('db.json', defaultData)
 
 const app = express()
 const port = 3000
@@ -10,6 +14,7 @@ app.get('/getResponses', (req, res) => {
 })
 
 app.post('/postResponse', async (req, res) => {
+  await db.update(({ posts }) => posts.push('hello world'))
   res.send('POST request to the homepage')
 })
 
